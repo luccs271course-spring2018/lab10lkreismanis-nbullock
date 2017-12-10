bfdd3b30fc105f5974879d96d293eac53d064bfc
Nathan Bullock <nbullock1@luc.edu>
2017-12-10 18:55:45 +0000

    Organized output

--- src/main/java/edu/luc/cs271/lab10/Main.java
+++ src/main/java/edu/luc/cs271/lab10/Main.java
@@ -46,42 +46,42 @@ public class Main {
     final Iterator<String> BF = new BreadthFirstIterator<>(GeoGraph, IL);
     while (BF.hasNext()) {
       final String state = BF.next();
-      System.out.println(state);
+      System.out.println("Breadth First " + state);
     }
     final Iterator<String> BF2 = new BreadthFirstIterator<>(GeoGraph, IN);
     while (BF2.hasNext()) {
       final String state = BF2.next();
-      System.out.println(state);
+      System.out.println("Breadth First 2 " + state);
     }
     final Iterator<String> CS = new ClosestFirstIterator<>(GeoGraph, IL);
     while (CS.hasNext()) {
       final String state = CS.next();
-      System.out.println(state);
+      System.out.println("Closest First " + state);
     }
     final Iterator<String> CS2 = new ClosestFirstIterator<>(GeoGraph, IN);
     while (CS2.hasNext()) {
       final String state = CS2.next();
-      System.out.println(state);
+      System.out.println("Closest First 2 " + state);
     }
     final Iterator<String> DF = new DepthFirstIterator<>(GeoGraph, IL);
     while (DF.hasNext()) {
       final String state = DF.next();
-      System.out.println(state);
+      System.out.println("Depth First " + state);
     }
     final Iterator<String> DF2 = new DepthFirstIterator<>(GeoGraph, IN);
     while (DF2.hasNext()) {
       final String state = DF2.next();
-      System.out.println(state);
+      System.out.println("Depth First 2 " + state);
     }
     final Iterator<String> RW = new RandomWalkIterator<>(GeoGraph, IL, false, 9);
     while (RW.hasNext()) {
       final String state = RW.next();
-      System.out.println(state);
+      System.out.println("Random Walk " + state);
     }
     final Iterator<String> RW2 = new RandomWalkIterator<>(GeoGraph, IN, false, 9);
     while (RW2.hasNext()) {
       final String state = RW2.next();
-      System.out.println(state);
+      System.out.println("Random Walk 2 " + state);
     }
 
     GreedyColoring colorGraph = new GreedyColoring(GeoGraph);
